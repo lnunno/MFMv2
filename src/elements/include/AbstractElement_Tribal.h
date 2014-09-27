c74e09c805df7b4bdee4c2749e79c237ac37ad00
lnunno <lucasnunno@gmail.com>
2014-09-27 10:14:06 -0600

    Updated Eclipse formatting changes.

--- src/elements/include/AbstractElement_Tribal.h
+++ src/elements/include/AbstractElement_Tribal.h
@@ -1,29 +1,29 @@
 /*                                              -*- mode:C++ -*-
-  AbstractElement_Tribal.h Tutorial template for a new Element
-  Copyright (C) 2014 The Regents of the University of New Mexico.  All rights reserved.
+ AbstractElement_Tribal.h Tutorial template for a new Element
+ Copyright (C) 2014 The Regents of the University of New Mexico.  All rights reserved.
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
-  USA
-*/
+ You should have received a copy of the GNU General Public License
+ along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+ USA
+ */
 
 /* <<TEMPLATE>> Replace doxygen and license information with your element information. */
 /**
-  \file   AbstractElement_Tribal.h Tribal base class.
-  \author Lucas L. Nunno
-  \date (C) 2014 All rights reserved.
-  \lgpl
+ \file   AbstractElement_Tribal.h Tribal base class.
+ \author Lucas L. Nunno
+ \date (C) 2014 All rights reserved.
+ \lgpl
  */
 #ifndef ELEMENT_TRIBAL_H /* <<TEMPLATE>> Replace header guard with your element name.*/
 #define ELEMENT_TRIBAL_H
@@ -41,135 +41,130 @@ using namespace std;
 namespace MFM
 {
 
-  template <class CC>
-  class AbstractElement_Tribal : public Element<CC>
+  template<class CC>
+  class AbstractElement_Tribal: public Element<CC>
   {
-    // Extract short names for parameter types
-    typedef typename CC::ATOM_TYPE T;
-    typedef typename CC::PARAM_CONFIG P;
+      // Extract short names for parameter types
+      typedef typename CC::ATOM_TYPE T;
+      typedef typename CC::PARAM_CONFIG P;
 
-  public:
-    // BEGIN ATOMIC PARAMETERS
-    // From the atomic parameters tutorial:
-    enum
-    {
-    	R = P::EVENT_WINDOW_RADIUS,
-        BITS = P::BITS_PER_ATOM,
+    public:
+      // BEGIN ATOMIC PARAMETERS
+      // From the atomic parameters tutorial:
+      enum
+      {
+        R = P::EVENT_WINDOW_RADIUS, BITS = P::BITS_PER_ATOM,
 
-        TRIBE_LENGTH = 4,
-        TRIBE_POSITION = BITS - TRIBE_LENGTH,
+        TRIBE_LENGTH = 4, TRIBE_POSITION = BITS - TRIBE_LENGTH,
 
-        TRIBAL_FIRST_FREE_POSITION = BITS - (TRIBE_LENGTH-1)
-    };
+        TRIBAL_FIRST_FREE_POSITION = BITS - (TRIBE_LENGTH - 1)
+      };
 
-    typedef BitField<BitVector<BITS>, TRIBE_LENGTH, TRIBE_POSITION> TribeBitField;
-    // END ATOMIC PARAMETERS
+      typedef BitField<BitVector<BITS>, TRIBE_LENGTH, TRIBE_POSITION> TribeBitField;
+      // END ATOMIC PARAMETERS
 
+      /*
+       * Tribe enum. This defines which tribes we can actually have.
+       */
+      enum Tribe
+      {
+        RED = 1, BLUE, GREEN, YELLOW, TRIBE_COUNT
+      };
 
-    /*
-     * Tribe enum. This defines which tribes we can actually have.
-     */
-    enum Tribe
-    {
-    	RED=1,
-    	BLUE,
-    	GREEN,
-    	YELLOW,
-    	TRIBE_COUNT
-    };
+    private:
 
-  private:
+      /* <<TEMPLATE>> Add any configurable parameters here. */
 
-    /* <<TEMPLATE>> Add any configurable parameters here. */
+    protected:
 
-  protected:
+      ElementParameterS32<CC> m_tribe;
 
-    ElementParameterS32<CC> m_tribe;
+      /**
+       * This is the shading of an element from its local tribe color
+       * in order to differentiate elements that are on the same tribe.
+       */
+      s32 m_elementGradient;
 
-    /**
-     * This is the shading of an element from its local tribe color
-     * in order to differentiate elements that are on the same tribe.
-    */
-    s32 m_elementGradient;
+      /**
+       * Get the amount that each element should be shaded by.
+       *
+       * It works like so:
+       *
+       * FINAL_COLOR = TRIBE_COLOR + ELEMENT_GRADIENT
+       */
+      s32 GetElementGradient() const
+      {
+        return m_elementGradient;
+      }
 
-    /**
-     * Get the amount that each element should be shaded by.
-     *
-     * It works like so:
-     *
-     * FINAL_COLOR = TRIBE_COLOR + ELEMENT_GRADIENT
-     */
-    s32 GetElementGradient() const
-    {
-    	return m_elementGradient;
-    }
+      /**
+       * Set the element gradient.
+       */
+      void SetElementGradient(s32 gradientValue)
+      {
+        m_elementGradient = gradientValue;
+      }
 
-    /**
-     * Set the element gradient.
-     */
-    void SetElementGradient(s32 gradientValue)
-    {
-    	m_elementGradient = gradientValue;
-    }
+    public:
 
-  public:
+      u32 GetTribe(const T& us) const
+      {
+        return TribeBitField::Read(this->GetBits(us));
+      }
 
-    u32 GetTribe(const T& us) const
-	{
-		return TribeBitField::Read(this->GetBits(us));
-	}
+      void SetTribe(T& us, const u32 tribe) const
+      {
+        TribeBitField::Write(this->GetBits(us), tribe & 0xf);
+      }
 
-	void SetTribe(T& us, const u32 tribe) const
-	{
-		TribeBitField::Write(this->GetBits(us), tribe & 0xf);
-	}
+      /*
+       * This supplies the tribe's color.
+       */
+      virtual u32 LocalPhysicsColor(const T& atom, u32 selector) const
+      {
+        u32 tribeNumber = GetTribe(atom);
+        switch (tribeNumber)
+        {
+          case RED:
+            return 0xFF880000 + GetElementGradient();
+          case BLUE:
+            return 0xFF000088 + GetElementGradient();
+          case GREEN:
+            return 0xFF008800 + GetElementGradient();
+          case YELLOW:
+            return 0xFF888800 + GetElementGradient();
+          default:
+            // Shouldn't get here, fail if we do.
+            FAIL(ILLEGAL_STATE);
+            break;
+        }
+        // Shouldn't get here, fail if we do.
+        FAIL(ILLEGAL_STATE);
+        return 0x0;
+      }
 
-    /*
-     * This supplies the tribe's color.
-     */
-    virtual u32 LocalPhysicsColor(const T& atom, u32 selector) const
-    {
-    	u32 tribeNumber = GetTribe(atom);
-    	switch(tribeNumber)
-    	{
-    	case RED:
-    		return 0xFF880000 + GetElementGradient();
-    	case BLUE:
-    		return 0xFF000088 + GetElementGradient();
-    	case GREEN:
-    		return 0xFF008800 + GetElementGradient();
-    	case YELLOW:
-    		return 0xFF888800 + GetElementGradient();
-    	default:
-    		// Shouldn't get here, fail if we do.
-    		FAIL(ILLEGAL_STATE);
-    		break;
-    	}
-    	// Shouldn't get here, fail if we do.
-    	FAIL(ILLEGAL_STATE);
-    	return 0x0;
-    }
+      u32 GetSelectedTribe()
+      {
+        return (u32) m_tribe.GetValue();
+      }
 
-    u32 GetSelectedTribe()
-    {
-    	return (u32) m_tribe.GetValue();
-    }
+      /**
+       * Test the other tribal element to see if it is in the same tribe as this element.
+       *
+       * TODO: Implement
+       */
+      bool IsInSameTribe(const AbstractElement_Tribal& us,
+          const AbstractElement_Tribal& otherGuy)
+      {
+        return false;
+      }
 
-    /**
-     * Test the other tribal element to see if it is in the same tribe as this element.
-     *
-     * TODO: Implement
-     */
-    bool IsInSameTribe(const AbstractElement_Tribal& us, const AbstractElement_Tribal& otherGuy)
-    {
-    	return false;
-    }
-
-    AbstractElement_Tribal(const UUID & uuid) : Element<CC>(uuid),
-    		m_tribe(this,"tribe","Tribe","This is the tribe that this element has.",RED,RED,TRIBE_COUNT-1,1),
-    		m_elementGradient(0)
-    {
-    }
+      AbstractElement_Tribal(const UUID & uuid) :
+          Element<CC>(uuid), m_tribe(this, "tribe", "Tribe",
+              "This is the tribe that this element has.", RED, RED,
+              TRIBE_COUNT - 1, 1), m_elementGradient(0)
+      {
+      }
 
   };
 
--- src/elements/include/Element_Base.h
+++ src/elements/include/Element_Base.h
@@ -1,29 +1,29 @@
 /*                                              -*- mode:C++ -*-
-  Element_Base.h Tutorial template for a new Element
-  Copyright (C) 2014 The Regents of the University of New Mexico.  All rights reserved.
+ Element_Base.h Tutorial template for a new Element
+ Copyright (C) 2014 The Regents of the University of New Mexico.  All rights reserved.
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
-  USA
-*/
+ You should have received a copy of the GNU General Public License
+ along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+ USA
+ */
 
 /* <<TEMPLATE>> Replace doxygen and license information with your element information. */
 /**
-  \file   Element_Base.h Base element for creating units.
-  \author Lucas L. Nunno
-  \date (C) 2014 All rights reserved.
-  \lgpl
+ \file   Element_Base.h Base element for creating units.
+ \author Lucas L. Nunno
+ \date (C) 2014 All rights reserved.
+ \lgpl
  */
 #ifndef Element_Base_H /* <<TEMPLATE>> Replace header guard with your element name.*/
 #define Element_Base_H
@@ -41,133 +41,132 @@ using namespace std;
 namespace MFM
 {
 
-  #define BASE_VERSION 1
+#define BASE_VERSION 1
 
-  template <class CC>
-  class Element_Base : public AbstractElement_Tribal<CC> /* <<TEMPLATE>> Replace class name with yours */
+  template<class CC>
+  class Element_Base: public AbstractElement_Tribal<CC> /* <<TEMPLATE>> Replace class name with yours */
   {
-    // Extract short names for parameter types
-    typedef typename CC::ATOM_TYPE T;
-    typedef typename CC::PARAM_CONFIG P;
+      // Extract short names for parameter types
+      typedef typename CC::ATOM_TYPE T;
+      typedef typename CC::PARAM_CONFIG P;
 
-    enum
-	{
-    	R = P::EVENT_WINDOW_RADIUS,
-    	BITS = P::BITS_PER_ATOM,
+      enum
+      {
+        R = P::EVENT_WINDOW_RADIUS, BITS = P::BITS_PER_ATOM,
 
-    	//////
-    	// Element state fields.
+        //////
+        // Element state fields.
 
-		GOLD_LENGTH = 8,
-		GOLD_POSITION = AbstractElement_Tribal<CC>::TRIBAL_FIRST_FREE_POSITION - GOLD_LENGTH
-	};
+        GOLD_LENGTH = 8,
+        GOLD_POSITION = AbstractElement_Tribal<CC>::TRIBAL_FIRST_FREE_POSITION
+            - GOLD_LENGTH
+      };
 
-    typedef BitField<BitVector<BITS>, GOLD_LENGTH, GOLD_POSITION> GoldBitField;
+      typedef BitField<BitVector<BITS>, GOLD_LENGTH, GOLD_POSITION> GoldBitField;
 
-  private:
+    private:
 
-    ElementParameterS32<CC> m_goldPerRes;
-    ElementParameterS32<CC> m_baseGoldCost;
-    ElementParameterS32<CC> m_baseCreateOdds;
+      ElementParameterS32<CC> m_goldPerRes;
+      ElementParameterS32<CC> m_baseGoldCost;
+      ElementParameterS32<CC> m_baseCreateOdds;
 
+    protected:
+      // We don't want the gold field to be accessible to the world.
+      u32 GetGoldCount(const T& us) const
+      {
+        return GoldBitField::Read(this->GetBits(us));
+      }
 
+      void SetGoldCount(T& us, const u32 goldCount) const
+      {
+        GoldBitField::Write(this->GetBits(us), goldCount & 0xff);
+      }
 
-  protected:
-    // We don't want the gold field to be accessible to the world.
-    u32 GetGoldCount(const T& us) const
-	{
-		return GoldBitField::Read(this->GetBits(us));
-	}
+    public:
 
-	void SetGoldCount(T& us, const u32 goldCount) const
-	{
-		GoldBitField::Write(this->GetBits(us), goldCount & 0xff);
-	}
+      /* <<TEMPLATE>> Replace class name with yours. Don't forget the '<CC>'. */
+      static Element_Base<CC> THE_INSTANCE;
 
-  public:
+      Element_Base() :
+          AbstractElement_Tribal<CC>(MFM_UUID_FOR("Base", BASE_VERSION)),
+          /* <<TEMPLATE>> Initialize all configurable parameters here. */
+          m_goldPerRes(this, "goldPRes", "Gold Per Res",
+              "The amount of gold produced for each Res collected.", 1, 10, 100,
+              1), m_baseGoldCost(this, "baseGoldCost", "Base Gold Cost",
+              "The cost (in gold) of producing a base.", 1, 5, 100, 1), m_baseCreateOdds(
+              this, "baseCreate", "Base Create Chance",
+              "The chance that a base will be attempted to be created.", 1, 5,
+              100, 1)
+      {
+        /* <<TEMPLATE>> Set atomic symbol and name for your element. */
+        Element<CC>::SetAtomicSymbol("Bs");
+        Element<CC>::SetName("Base");
+        AbstractElement_Tribal<CC>::SetElementGradient(0x00110000);
+      }
 
-    /* <<TEMPLATE>> Replace class name with yours. Don't forget the '<CC>'. */
-    static Element_Base<CC> THE_INSTANCE;
-
-    Element_Base()
-      : AbstractElement_Tribal<CC>(MFM_UUID_FOR("Base", BASE_VERSION)),
-        /* <<TEMPLATE>> Initialize all configurable parameters here. */
-        m_goldPerRes(this, "goldPRes", "Gold Per Res",
-                  "The amount of gold produced for each Res collected.", 1, 10, 100, 1),
-        m_baseGoldCost(this, "baseGoldCost", "Base Gold Cost",
-        		  "The cost (in gold) of producing a base.", 1, 5, 100, 1),
-        m_baseCreateOdds(this,"baseCreate", "Base Create Chance",
-        		"The chance that a base will be attempted to be created.", 1, 5, 100, 1)
-    {
-      /* <<TEMPLATE>> Set atomic symbol and name for your element. */
-      Element<CC>::SetAtomicSymbol("Bs");
-      Element<CC>::SetName("Base");
-      AbstractElement_Tribal<CC>::SetElementGradient(0x00110000);
-    }
-
-    /*
+      /*
        <<TEMPLATE>> Set how likely your element is to be moved by another element. See
-                    Element.h for details.
-    */
-    virtual u32 PercentMovable(const T& you,
-                               const T& me, const SPoint& offset) const
-    {
-      return 0;
-    }
+       Element.h for details.
+       */
+      virtual u32 PercentMovable(const T& you, const T& me,
+          const SPoint& offset) const
+      {
+        return 0;
+      }
 
-    /* <<TEMPLATE>> This color will be the default rendering color for your element. */
-    virtual u32 DefaultPhysicsColor() const
-    {
-      return 0xff0000ff;
-    }
+      /* <<TEMPLATE>> This color will be the default rendering color for your element. */
+      virtual u32 DefaultPhysicsColor() const
+      {
+        return 0xff0000ff;
+      }
 
-    /*
+      /*
        <<TEMPLATE>> This color will be the color rendered when your
-                    element is rendered in lowlighting.
-     */
-    virtual u32 DefaultLowlightColor() const
-    {
-      return 0xff0000ff;
-    }
+       element is rendered in lowlighting.
+       */
+      virtual u32 DefaultLowlightColor() const
+      {
+        return 0xff0000ff;
+      }
 
-    /*
-      <<TEMPLATE>> This is a short description of your element.
-     */
-    virtual const char* GetDescription() const
-    {
-      return "This is a base that creates other units and collects Res.";
-    }
+      /*
+       <<TEMPLATE>> This is a short description of your element.
+       */
+      virtual const char* GetDescription() const
+      {
+        return "This is a base that creates other units and collects Res.";
+      }
 
-    static const u32 TYPE()
-    {
-    	return THE_INSTANCE.GetType();
-    }
+      static const u32 TYPE()
+      {
+        return THE_INSTANCE.GetType();
+      }
 
-    virtual const T& GetDefaultAtom() const
-    {
-    	static T defaultAtom(TYPE(), 0, 0, 0);
-    	u32 tribeValue = this->m_tribe.GetValue();
-    	this->SetTribe(defaultAtom, tribeValue);
-    	return defaultAtom;
-    }
+      virtual const T& GetDefaultAtom() const
+      {
+        static T defaultAtom(TYPE(), 0, 0, 0);
+        u32 tribeValue = this->m_tribe.GetValue();
+        this->SetTribe(defaultAtom, tribeValue);
+        return defaultAtom;
+      }
 
-    /*
-      <<TEMPLATE>> This method is executed every time an atom of your
-                   element is chosen for an event. See the tutorial in
-                   the wiki for further information.
-     */
-    virtual void Behavior(EventWindow<CC>& window) const
-    {
-    	T self = window.GetCenterAtom();
+      /*
+       <<TEMPLATE>> This method is executed every time an atom of your
+       element is chosen for an event. See the tutorial in
+       the wiki for further information.
+       */
+      virtual void Behavior(EventWindow<CC>& window) const
+      {
+        T self = window.GetCenterAtom();
 //    	u32 tribe = this->GetTribe(self);
-    }
+      }
 
   };
 
   /*
-     <<TEMPLATE>> Rename the class names here to the class name of your element.
-  */
-  template <class CC>
+   <<TEMPLATE>> Rename the class names here to the class name of your element.
+   */
+  template<class CC>
   Element_Base<CC> Element_Base<CC>::THE_INSTANCE;
 
 }
